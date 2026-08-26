c51ed485bfd01ad94bca40abe92de2de17e2f34c
andresco91-crypto <andresco91@gmail.com>
2026-08-26 15:12:52 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ink: '#0a0e1a',
+        panel: '#0e1420',
+        card: '#111827',
+        line: '#1f2a3d',
+        muted: '#8b93a7',
+        brand: {
+          DEFAULT: '#2f6fed',
+          dark: '#2558c4',
+          light: '#5b8ff9',
+        },
+      },
+      fontFamily: {
+        display: ['"Space Grotesk"', 'system-ui', 'sans-serif'],
+        sans: ['"Inter"', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
